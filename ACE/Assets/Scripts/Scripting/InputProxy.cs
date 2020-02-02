e2f7a41697053c2fd7a28b3d1be4d35555805ff0
Ryan McCampbell <rmccampbell7@gmail.com>
2020-02-01 19:15:43 -0500

    Unused python scripting code

---
+++ ACE/Assets/Scripts/Scripting/InputProxy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class InputProxy
+{
+    public bool left()
+    {
+        return Input.GetAxisRaw("Horizontal") < 0;
+    }
+
+    public bool right()
+    {
+        return Input.GetAxisRaw("Horizontal") > 0;
+    }
+
+    public bool space()
+    {
+        return Input.GetKeyDown(KeyCode.Space);
+    }
+}
---
+++ ACE/Assets/Scripts/Scripting/PlayerScriptExecutor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScriptExecutor : ScriptExecutor
+{
+    protected override void PreExec()
+    {
+        print("PreExec");
+        scope.SetVariable("player", new PlayerProxy(gameObject));
+        scope.SetVariable("input", new InputProxy());
+    }
+
+    protected override void PostExec()
+    {
+        print("PostExec");
+    }
+
+    public class PlayerProxy
+    {
+        private GameObject player;
+        public int x = 3;
+
+        public PlayerProxy(GameObject player)
+        {
+            this.player = player;
+        }
+
+        public void go_left()
+        {
+            print("go_left");
+            player.transform.position += Vector3.left * Time.deltaTime;
+        }
+
+        public void go_right()
+        {
+            print("go_right");
+            player.transform.position += Vector3.right * Time.deltaTime;
+        }
+
+        public void jump()
+        {
+            print("jump");
+            player.transform.position += Vector3.up * 2;
+        }
+    }
+}
