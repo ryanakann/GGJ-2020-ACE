094229afb617d0edce3f5da778758901a10a966c
Ryan Kann <rak3me@virginia.edu>
2020-02-02 16:39:38 -0500

    Menu

---
+++ ACE/Assets/Blink.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Blink : MonoBehaviour {
+    private SpriteRenderer sr;
+
+    public float waitTime = 0.6f;
+
+    private void Awake () {
+        sr = GetComponent<SpriteRenderer>();
+        StartCoroutine(BlinkCR());
+    }
+
+    private void Update () {
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    IEnumerator BlinkCR () {
+        while (true) {
+            yield return new WaitForSeconds(waitTime);
+            sr.enabled = !sr.enabled;
+        }
+    }
+}
